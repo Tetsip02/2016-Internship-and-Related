5b8f3c6edb8921d08d3633ea43ffc560b47e1ac4
Sergio Azizi <sergioazizi@176.115.248.10.in-addr.arpa.private.cam.ac.uk>
2016-09-09 12:58:25 +0100

    deleted redundant files

--- regression/ridgeReg_plot.m
+++
@@ -1,8 +0,0 @@
-load('X.out');
-load('y.out');
-load('theta_ridgeReg.out');
-plot(X,y,'.b');
-regline_X = min(X):0.1:max(X);
-regline_y = theta_ridgeReg(2)*regline_X + theta_ridgeReg(1);
-hold on;
-plot(regline_X,regline_y);
